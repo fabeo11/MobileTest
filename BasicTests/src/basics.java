11677bd4467ecea7991e799c182acd11d666f674
Fabio.Masao <f.masaoishizu@gmail.com>
2022-02-07 10:34:47 -0300

    selecting elements dif ways

--- BasicTests/src/basics.java
+++ BasicTests/src/basics.java
@@ -14,7 +14,11 @@ public class basics extends base {
 		//xpath syntax:  tagName[@attribute='value']
 		
 		driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
-		
+		driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+		driver.findElementById("android:id/checkbox").click();
+		driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+		driver.findElementByClassName("android.widget.EditText").sendKeys("Naruto Uzumaki");
+		driver.findElementsByClassName("android.widget.Button").get(1).click();
 	}
 	
 	
